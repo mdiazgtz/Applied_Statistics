4ffb120634b9fb42164a4eecd2850f1b50ec2320
Michelle Díaz <78995651+mdiazgtz@users.noreply.github.com>
2021-08-11 01:05:09 -0500

    Add files via upload

---
+++ 1.R
@@ -0,0 +1,18 @@
+#SUMAS, RESTAS Y POTENCIAS
+5+2 #suma
+80-12 #resta
+7*3^13 #operacion
+4**3#potencia
+87%%2 #residuo
+
+150/3 #division
+
+#declarando variables
+a = 5
+b =6
+c =14
+
+x1 = (b + ((b**2 - 4*a*c)))/(2*a)
+x2 = (b - ((b**2 - 4*a*c)))/(2*a)
+x1
+x2
